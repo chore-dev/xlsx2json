77aec3616968b6bb1d0c3ca03d04624c4b38d734
Issac Li <issac.li.523@gmail.com>
2024-09-24 18:22:24 +0800

    feat: added isArray util

--- src/shared-utilities/common/array.ts
+++ src/shared-utilities/common/array.ts
@@ -1,3 +1,7 @@
-export const arrayWrap = <Input>(input: Input | Input[]): Input[] => {
+export const arrayWrap = <Input>(input: Input | Array<Input>): Array<Input> => {
   return Array.isArray(input) ? input : [input];
+};
+
+export const isArray = (input: unknown): input is Array<unknown> => {
+  return Array.isArray(input);
 };
