52620b43cc61b13eb6c427b59a6f2c2cfd50ef7f
Issac Li <issac.li.523@gmail.com>
2024-09-25 22:19:23 +0800

    feat: added array delimiter to replacePlaceholders utility

--- src/shared-utilities/common/string.ts
+++ src/shared-utilities/common/string.ts
@@ -1,13 +1,13 @@
 export const replacePlaceholders = (
   input: string,
   values: Record<string, unknown> = {},
-  _delimiter?: { prefix: string; suffix: string }
+  _delimiter?: { array?: string; prefix?: string; suffix?: string }
 ) => {
-  const delimiter = Object.assign({ prefix: '{{', suffix: '}}' }, _delimiter);
+  const delimiter = Object.assign({ array: ', ', prefix: '{{', suffix: '}}' }, _delimiter);
 
   return Object.entries(values).reduce((replacedInput, [key, _value]) => {
     let value = _value;
-    if (Array.isArray(value)) value = value.join(', ');
+    if (Array.isArray(value)) value = value.join(delimiter.array);
 
     return replacedInput.replace(
       new RegExp(`${delimiter.prefix}${key}${delimiter.suffix}`, 'g'),
