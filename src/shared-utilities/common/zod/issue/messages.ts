c9852b1559322296d53ec0be8d3f9bee5afab6c5
Issac Li <issac.li.523@gmail.com>
2024-09-26 18:06:13 +0800

    feat: implement ZodIssueCode.invalid_string custom message

--- src/shared-utilities/common/zod/issue/messages.ts
+++ src/shared-utilities/common/zod/issue/messages.ts
@@ -7,14 +7,13 @@
 //   - ZodIssueCode.invalid_union_discriminator
 // + Won't customize
 //   - ZodIssueCode.custom
-// + Pending
-//   - ZodIssueCode.invalid_string
 
 import { ZodIssue, ZodIssueCode } from 'zod';
 
 import { isFunction } from '../../is';
 import { isKeyOf } from '../../object';
 
+import getInvalidStringIssueMessage from './types/string';
 import get2BigOr2SmallIssueMessage from './types/tooBigOrTooSmall';
 
 const GENERAL_MESSAGE = '[{{code}}] Something went wrong!';
@@ -23,6 +22,10 @@ const CUSTOMIZED_MESSAGES = {
   [ZodIssueCode.invalid_date]: 'Expected a valid date',
   [ZodIssueCode.invalid_enum_value]: 'Expected `{{options}}` but got `{{received}}` instead',
   [ZodIssueCode.invalid_literal]: 'Expected `{{expected}}` but got `{{received}}` instead',
+  [ZodIssueCode.invalid_string]: issue => {
+    if (issue.code !== ZodIssueCode.invalid_string) return;
+    return getInvalidStringIssueMessage(issue);
+  },
   [ZodIssueCode.invalid_type]: 'Expected `{{expected}}` but got `{{received}}` instead',
   [ZodIssueCode.invalid_union]: 'Error(s) detected. See `unionErrors` for specifics',
   [ZodIssueCode.not_finite]: 'Expected a finite number',
---
+++ src/shared-utilities/common/zod/issue/types/string.ts
@@ -0,0 +1,44 @@
+import { StringValidation, ZodInvalidStringIssue } from 'zod';
+
+import { isObject } from '../../../is';
+import { isKeyOf } from '../../../object';
+import { replacePlaceholders } from '../../../string';
+
+const VALIDATION_MESSAGES = {
+  endsWith: 'to end with `{{endsWith}}`',
+  includes: 'to include `{{includes}}`',
+  startsWith: 'to start with `{{startsWith}}`'
+} as const satisfies Partial<Record<Extract<Validation, string>, string>>;
+
+const getInvalidStringIssueMessage = (issue: ZodInvalidStringIssue) => {
+  const { message, validation: variables } = issue;
+
+  const hasVariables = isObject(variables);
+  const type = hasVariables ? Object.keys(variables)[0] : variables;
+
+  if (!hasVariables) return `Expected a valid \`${type}\``;
+
+  if (type && isKeyOf(VALIDATION_MESSAGES, type)) {
+    let message = `Expected the value ${VALIDATION_MESSAGES[type]}`;
+
+    if (hasVariables && 'position' in variables) {
+      message += ` at position \`${variables.position}\``;
+    }
+
+    return replacePlaceholders(message, hasVariables ? variables : {});
+  }
+
+  return message;
+};
+
+export default getInvalidStringIssueMessage;
+
+type UnionToIntersection<U> = (U extends unknown ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
+type Validation =
+  | Extract<StringValidation, string>
+  | Exclude<keyof UnionToIntersection<Exclude<StringValidation, string>>, 'position'>;
